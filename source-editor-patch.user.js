e01e69ccd68cdc56d6906551a6f7f8b51e3ce460
jtdjdu6868 <99466048+jtdjdu6868@users.noreply.github.com>
2026-06-04 17:08:06 +0800

    update script name

--- source-editor-patch.user.js
+++ source-editor-patch.user.js
@@ -1,5 +1,6 @@
 // ==UserScript==
-// @name         巴哈姆特哈拉區原始碼編輯補丁
+// @name         Source editor patch for Bahamut forum post1.php
+// @name:zh-TW   巴哈姆特哈拉區原始碼編輯補丁
 // @namespace    https://jtdjdu6868.com/
 // @version      1.0
 // @description  臨時補上巴哈姆特哈拉區發文編輯器的原始碼切換功能
