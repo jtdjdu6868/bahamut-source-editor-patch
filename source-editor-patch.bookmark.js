4791849a89b54345d9cc872fb6ff2db7720d9387
jtdjdu6868 <99466048+jtdjdu6868@users.noreply.github.com>
2026-06-04 17:33:53 +0800

    Add foolproof for bookmark script

--- source-editor-patch.bookmark.js
+++ source-editor-patch.bookmark.js
@@ -1,4 +1,18 @@
 javascript:(function() {
+    if (window.__bahamutSourceEditorPatchRunOnce) {
+        return;
+    }
+    Object.defineProperty(window, "__bahamutSourceEditorPatchRunOnce", {
+        value: true,
+        configurable: false,
+        writable: false,
+        enumerable: false
+    });
+    if (!/^https:\/\/forum\.gamer\.com\.tw\/post1\.php/.test(location.href))
+    {
+        alert("本書籤只能在巴哈姆特哈拉區的發文頁面使用，請確認網址是否正確");
+        return;
+    };
     const editorGroup = jQuery("<div>", {
         class: "top-editor__icongroup top-editor__icongroup-fe_source1"
     });
